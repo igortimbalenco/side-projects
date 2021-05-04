cb9c774f17cf226ddd01cb5e95606c01e67edf7e
Igor <igor.timbalenco@gmail.com>
2021-05-04 22:58:16 +0300

    TODO app first commit

---
+++ ToDoApp/src/Interaction.java
@@ -0,0 +1,69 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Interaction {
+
+    public Scanner scanner = new Scanner(System.in);
+    public Map<Integer, Task> map = new HashMap<>();
+
+    public void delete(){
+        System.out.print("Please enter task ID: ");
+        Integer taskID = scanner.nextInt();
+        map.remove(taskID);
+    }
+
+    public void addNewTask(){
+        System.out.print("\nInput uniq id: ");
+        Integer id = scanner.nextInt();
+        System.out.print("\nInput task name: ");
+        String name = scanner.nextLine();
+        System.out.print("\nInput task description: ");
+        String description = scanner.nextLine();
+        System.out.print("\nInput date in the following format yyyy-MM-dd: ");
+        String stringDate = scanner.nextLine();
+        Date date = parseDate(stringDate);
+        map.put( id, new Task(id, name, description, date, false));
+    }
+
+    public void updateTask(){
+        System.out.println("Please enter task id:");
+        Integer id = scanner.nextInt();
+        System.out.println("Is the task done?");
+        Boolean reply = scanner.nextBoolean();
+        map.get(id).setDone(reply);
+    }
+
+    public int selectOption(){
+        System.out.println("     Welcome to TODO List App  ");
+        System.out.println("Available options: ");
+        System.out.println("[1] View All tasks");
+        System.out.println("[2] Add new task");
+        System.out.println("[3] Delete a task");
+        System.out.println("[4] Update a task");
+        System.out.println("[0] Exit\n\n");
+        return scanner.nextInt();
+    }
+
+    public static Date parseDate(String date) {
+        try {
+            return new SimpleDateFormat("yyyy-MM-dd").parse(date);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+    public void printMap(){
+        if (!map.isEmpty()){
+            String prettyMap = map.entrySet().stream().map(e -> e.getValue() + "\n").collect(Collectors.joining(" "));
+            System.out.println(prettyMap);
+        } else {
+            System.out.println("There are no tasks yet");
+        }
+    }
+
+}
---
+++ ToDoApp/src/Task.java
@@ -0,0 +1,68 @@
+import java.util.Date;
+
+public class Task {
+    private int id;
+    private String name;
+    private String description;
+    private Date date;
+    private boolean done;
+
+    public Task(int id, String name, String description, Date date, boolean done) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.done = done;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", date=" + date +
+                ", done=" + done +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
